acd6d88e8ac9d9e14251ec4836e7b1558d819e86
Clem <clement.venot@gmail.com>
2019-12-14 20:41:02 +0100

    'same'

--- amazonSearch/views.py
+++ amazonSearch/views.py
@@ -7,7 +7,7 @@ def index(response, id):
     for i in range(3):
         myDict[i] = i+1
     ls = ToDoList.objects.get(id=id) # gets number from url to get ToDoList item with id passed as parameter
-    return render(response, 'amazonSearch/base.html', {})
+    return render(response, 'amazonSearch/list.html', {'ls':ls})
 
     #item = ls.item_set.get(id=1)
     #return HttpResponse('<h1>%s</h1><br></br><p>%s</p>' %(ls.name, str(item.text)))
