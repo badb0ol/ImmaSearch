11cf962cf15f9de8c983f397454414ecb042dbde
Clem <clement.venot@gmail.com>
2019-12-14 20:11:51 +0100

    added page base

--- amazonSearch/views.py
+++ amazonSearch/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-def index(response, name):
-    ls = ToDoList.objects.get(name=name) # gets number from url to get ToDoList item with id passed as parameter
-    item = ls.item_set.get(id=1)
-    return HttpResponse('<h1>%s</h1><br></br><p>%s</p>' %(ls.name, str(item.text)))
+def index(response, id):
+    myDict = {}
+    for i in range(3):
+        myDict[i] = i+1
+    ls = ToDoList.objects.get(id=id) # gets number from url to get ToDoList item with id passed as parameter
+    return render(response, 'amazonSearch/base.html', {})
+
+    #item = ls.item_set.get(id=1)
+    #return HttpResponse('<h1>%s</h1><br></br><p>%s</p>' %(ls.name, str(item.text)))
 
 def home(response):
-    pass
+    return render(response, 'amazonSearch/home.html', {})
